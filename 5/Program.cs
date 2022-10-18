77bbfd061de5fa9d4a0f5a8eb85d9493f7b99de3
kate <cvetok_spb@bk.ru>
2022-10-18 12:40:16 +0300

    Задача-5

---
+++ 5/Program.cs
@@ -0,0 +1,25 @@
+﻿// Найти сумму чисел одномерного массива стоящих на нечетной позиции
+int[] array = new int[7];
+int n = 0; // количество элементов, стоящих на нечетной позиции
+int sum =0 ;
+int i = 0;
+for ( i = 0;i<7; i++)
+    array[i] = new Random().Next(0, 100);
+Console.WriteLine ("Ваш массив ");
+
+for ( i = 0;i<7; i++)
+    Console.WriteLine ($"{array[i]} ");
+    Console.WriteLine();
+
+for ( i = 0;i<7; i++)
+{
+    if (i % 2 ==0)
+    {
+        n++;
+    }
+    else if (i % 2 !=0)
+    sum = array[i]+sum;
+}
+
+Console.WriteLine ($"Количество элементов, стоящих на нечетной позиции {n} ");
+Console.WriteLine ($"Сумма элементов, стоящих на нечетной позиции {sum} ");
